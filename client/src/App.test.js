e9bb8e08dde975ab8c3c74c3aec676ec4d811a4a
noahfranco <noahfra@gmail.com>
2019-08-09 16:19:07 -0400

    Ran two successful test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from "@testing-library/react";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe("<App />", () => {
+  
+  // Unit Test 
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+// Element Test 
+  it("renders page title", () => {
+    const app = render(<App />)
+
+    app.getByText(/This is a Happy Place/i)
+  })
+
+// // event test 
+// it("the submit button works", () => { 
+// const button = redner(<UserForm />); 
+
+// button.
+// })
+})
+
